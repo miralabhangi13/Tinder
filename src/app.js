370d846ad2f3b868dab1388f6b80a0dbab543358
Abhangi Miral <miralabhangi@gmail.com>
2025-09-30 23:50:21 +0530

    writing-middlewears

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express=require("express");
 const app=express();
-app.get("/user",(req,res)=>{
-    res.send({"first name":"abhngi","last name":"miral"});
+const{adminAuth}=require("./middlewears/auth");
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);
+//     res.send({"first name":"abhngi","last name":"miral"});
+// });
+app.use("/admin",adminAuth);
+app.get("/admin/getAllData",(req,res)=>{
+        res.send("All data getting!");
 });
-app.post("/user",(req,res)=>{
-    res.send("data saved to data base");
-});
-app.delete("/user",(req,res)=>{
-    res.send("data deleted succesfully");
-});
-app.use("/user",(req,res)=>{
-    res.send("Hello from server");
+app.get("/admin/deleteData",(req,res)=>{
+    res.send("data delted");
 });
 app.listen(6969,()=>{
     console.log("Server is listening");
