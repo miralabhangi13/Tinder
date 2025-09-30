9d4af160f3862e4a3e99eeefc93a4063bf9ffcf3
Abhangi Miral <miralabhangi@gmail.com>
2025-09-30 19:52:01 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express=require("express");
 const app=express();
-app.use("/test",(req,res)=>{
-    res.send("Hello from server");
+app.get("/user",(req,res)=>{
+    res.send({"first name":"abhngi","last name":"miral"});
 });
-app.use("/",(req,res)=>{
-    res.send("Hello");
+app.post("/user",(req,res)=>{
+    res.send("data saved to data base");
+});
+app.delete("/user",(req,res)=>{
+    res.send("data deleted succesfully");
+});
+app.use("/user",(req,res)=>{
+    res.send("Hello from server");
 });
 app.listen(6969,()=>{
     console.log("Server is listening");
