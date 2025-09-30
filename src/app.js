bdcdae7f128f0a8ddf089650a7be386bce3781d3
Abhangi Miral <miralabhangi@gmail.com>
2025-10-01 00:11:21 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -5,13 +5,25 @@ const{adminAuth}=require("./middlewears/auth");
 //     console.log(req.query);
 //     res.send({"first name":"abhngi","last name":"miral"});
 // });
-app.use("/admin",adminAuth);
+// app.use("/admin",adminAuth);
 app.get("/admin/getAllData",(req,res)=>{
-        res.send("All data getting!");
+        try{
+            throw Error("ssgsj");
+            res.send("All data getting!");
+        }catch(err){
+            res.send("wrong");
+        }
+        // throw Error("fbjws");
+        // res.send("getting data!");
 });
-app.get("/admin/deleteData",(req,res)=>{
-    res.send("data delted");
-});
+// app.use("/",(err,req,res,next)=>{
+//     if(err){
+//         res.send("somthing is wrong");
+//     }
+// });
+// app.get("/admin/deleteData",(req,res)=>{
+//     res.send("data delted");
+// });
 app.listen(6969,()=>{
     console.log("Server is listening");
 });
