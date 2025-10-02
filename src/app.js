db7c36e341b95790d1cac278c2ac495ff83c8901
Abhangi Miral <miralabhangi@gmail.com>
2025-10-02 10:31:34 +0530

    dynamic-data in singup api

--- src/app.js
+++ src/app.js
@@ -2,13 +2,9 @@ const express=require("express");
 const app=express();
 const {connectDB}=require("./config/database")
 const User=require("./models/user");
+app.use(express.json());
 app.post("/signup",async (req,res)=>{
-    const user=new User({
-        firstName:"Abhangi",
-        lastName:"Miral",
-        email:"miralabhangi@gmail.com",
-        passWord:"ghghghg",
-    });
+    const user=new User(req.body);
     try{
         await user.save();
         res.send("User Added succuesfully");
